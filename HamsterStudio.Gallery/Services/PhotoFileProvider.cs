31cd0873fb54dfa639277c6566e13b601b80470c
zhaoliangcai <zhaoliangcai@xyuauto.com>
2025-06-10 13:33:39 +0800

    rsc

---
+++ HamsterStudio.Gallery/Services/PhotoFileProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.FileProviders.Physical;
+using Microsoft.Extensions.Primitives;
+using System.IO;
+
+namespace HamsterStudio.Gallery.Services;
+
+internal class PhotoFileProvider : IFileProvider
+{
+    public IDirectoryContents GetDirectoryContents(string subpath)
+    {
+        var dirCont = new DirectoryInfo(subpath);
+        return new PhysicalDirectoryInfo(dirCont);
+    }
+
+    public IFileInfo GetFileInfo(string subpath)
+    {
+        var fileInfo = new FileInfo(subpath);
+        return new PhysicalFileInfo(fileInfo);
+    }
+
+    public IChangeToken Watch(string filter)
+    {
+        throw new NotImplementedException();
+    }
+}
