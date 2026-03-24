23ad259ed73273877bf7ea7311b6f756144ae502
swx12138 <zhaolc_bzh@163.com>
2026-03-24 22:48:49 +0800

    upd

--- HamsterStudioNative/Image/PixelData/PopularColors.hpp
+++ HamsterStudioNative/Image/PixelData/PopularColors.hpp
@@ -4,7 +4,7 @@
 
 namespace PantoneColors
 {
-	const cv::Scalar YearColor_2026_CloudDancer { 0xf5 , 0xf5, 0xf5 };		// PANTONE 11-4201 TCX
+	const cv::Scalar YearColor_2026_CloudDancer { 0xf5, 0xf5, 0xf5 };		// PANTONE 11-4201 TCX
 	const cv::Scalar YearColor_2025_MochaMousse { 100 , 119, 164 };		// PANTONE 17-1230 : Capturing a Global Mood of Connection, Comfort, and Harmony
 	const cv::Scalar YearColor_2024_PeachFuzz { 152 , 190, 255 };		// PANTONE 13-1023 : Embrace the Warmth
 	const cv::Scalar YearColor_2023_VivaMagenta { 0x49 , 0x26, 0xbb };	// PANTONE 18-1750 : Welcome to	the Magentaverse
